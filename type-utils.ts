62c235e0144c25b4d95c161fb8d427d318c3a7be
Martin Kamír <martin.k.kamir@gmail.com>
2025-01-08 17:21:50 +0100

    add vitest and update type Equal

--- type-utils.ts
+++ type-utils.ts
@@ -1,2 +1,7 @@
-export type Equal<X, Y> = X extends Y ? (Y extends X ? true : false) : false;
 export type Expect<T extends true> = T;
+
+export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
+    T
+>() => T extends Y ? 1 : 2
+    ? true
+    : false;
