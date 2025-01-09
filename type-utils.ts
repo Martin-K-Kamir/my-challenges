287c87ddbf731da35101fda9a5289d148cd3a7b5
Martin Kamír <martin.k.kamir@gmail.com>
2025-01-10 00:04:55 +0100

    fix type utils

--- type-utils.ts
+++ type-utils.ts
@@ -1,7 +1,2 @@
+export type Equal<X, Y> = X extends Y ? (Y extends X ? true : false) : false;
 export type Expect<T extends true> = T;
-
-export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
-    T
->() => T extends Y ? 1 : 2
-    ? true
-    : false;
